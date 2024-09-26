e7ec8c4543bb1de14cd1f8263803bfba88c42733
Ferxi14 <fernandogr0309@gmail.com>
2024-09-26 12:16:34 +0200

    index

--- index.php
+++ index.php
@@ -1,13 +1,24 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Bingo</title>
 </head>
+
 <body>
     <?php
-        
+    $cartones = array(array(), array(), array(), array());
+    $players = array(
+        ['yayo1'] => $cartones,
+        ['yayo2'] => $cartones,
+        ['yayo3'] => $cartones,
+        ['yayo4'] => $cartones
+    );
+    var_dump($players);
+
     ?>
 </body>
+
 </html>
