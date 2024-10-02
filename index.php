18d0ba7099f088a50b254cb982df339f7843c198
unknown <javierre13@hotmail.com>
2024-10-02 13:02:28 +0200

    fix: modularizado cant. jugadores, carones, bolas y tam. cartones

--- index.php
+++ index.php
@@ -1,134 +1,153 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Bingou</title>
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+        <?php
+            // AJUSTES DEL JUEGO [JUGADORES, CARTONES, TAMAÑO_CARTONES, BOLAS]
+            $ajustes = [4, 3, 15, 60];
+            comprobarAjustes($ajustes);
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Bingo</title>
-    <link rel="stylesheet" href="style.css">
-</head>
+            // EJECUCIÓN DEL JUEGO
+            $players = array();
+            crearJugadores($players, $ajustes);
 
-<body>
-    <?php
-    ini_set('max_execution_time', 1); 
+            imprimirCartones($players);
 
-    $players = array(
-        "yayo1" => array(),
-        "yayo2" => array(),
-        "yayo3" => array(),
-        "yayo4" => array()
-    );
+            $bolas = array();
+            crearBolas($bolas, $ajustes);
 
-    // RELLENA LOS CARTONES A TODOS LOS JUGADORES
-    foreach($players as $p => $i){
-        rellenarCarton($players[$p]);
-    } 
+            iniciarJuego($players, $bolas, $ajustes);
 
-    // INSERTA 3 ARRAYS LLENOS DE NUMEROS EN UN ARRAY QUE SE LE PASA POR PARAMETRO
-    function rellenarCarton(&$play){
-        for($i = 0; $i<3; $i++){
-            $carton = array();
-            insertNum($carton);
-            array_push($play,$carton);
-        }
-    };
+            /* ******************************** */
+            /*            FUNCIONES             */
+            /* ******************************** */
 
-    // MODIFICA UN ARRAY VACIO DANDOLE 15 NUMEROS ALEATORIOS DEL 1 AL 60 QUE NO SE REPITEN
-    function insertNum(&$arr){
-        $numExist = false;
-        $num = 0;
-        while(count($arr) < 15){
-            $num = rand(1,60);
-            $numExist = false;
-            foreach($arr as $i){
-                if($num == $i){
-                    $numExist = true;
+            // COMPROBAR AJUSTES
+            function comprobarAjustes($ajustes){
+                for ($i = 0; $i < count($ajustes); $i++)
+                    if ($ajustes[$i] < 1) {
+                        echo "<h2>Todos los ajustes deben tener un valor mínimo de 1</h2>";
+                        exit;
+                    }
+
+                if ($ajustes[2] > $ajustes[3]) {
+                    echo "<h2>El número de cartones no puede ser mayor que el número de bolas.</h2>";
+                    exit;
                 }
             }
-            if(!$numExist){
-                array_push($arr,$num);
 
+            /* ******************************** */
+
+            // CREAR JUGADORES Y SUS CARTONES
+            function crearJugadores(&$players, $ajustes) {
+                for ($i = 1; $i <= $ajustes[0]; $i++)
+                    $players["yayo" . $i ] = array();
+                
+                foreach($players as $player => $player)
+                    crearCartones($players[$player], $ajustes);
             }
-        }
-    }
-    imprimirCartones($players);
-    $bolas = array();
-    $hayGanador = false;
-    $ganador = "";
-    do {
-        $hayGanador = sacarBola($players, $bolas, $ganador);
-    } while(!$hayGanador);
-    echo "<h1>Ganador: ". $ganador . "</h1>";
-    imprimirCartones($players);
-    /**************************************************/
-    function sacarBola(&$players, &$bolas, &$ganador) {
-        $num = 0;
-
-        do {
-            $num = rand(1, 60);
-        } while(in_array($num, $bolas) && count($bolas) < 60);
-
-        echo "<img src='images/". $num . ".PNG' height='70'>";
-
-        count($bolas)<60 ? array_push($bolas, $num) : "";
-        comprobarBolaCartones($players, $num);
-        $hayGanador = comprobarGanador($players, $ganador);
-
-        return $hayGanador;
-    }
-    /**************************************************/
-    function imprimirCartones($players)
-    {
-        echo "<div class='container'>";
-        foreach ($players as $player => $cartones) {
-            echo "<div class='jugador'><h3>" . $player . " </h3>";
-            foreach ($cartones as $num => $carton) {
-                $num++;
-                echo "<table border='1'><caption> Carton " . $num . "</caption><tr>";
-                for ($i = 0; $i < 15; $i++) {
-                    if ($i % 5 == 0 && $i != 0) {
-                        echo "</tr> <tr> <td>" . $carton[$i] . "</td>";
-                    } else {
-                        echo "<td>" . $carton[$i] . "</td>";
+        
+            // CREAR LOS CARTONES DE UN JUGADOR
+            function crearCartones(&$player, $ajustes){
+                for($i = 0; $i < $ajustes[1]; $i++){
+                    $carton = array();
+                    rellenarCarton($carton, $ajustes);
+                    array_push($player,$carton);
+                }
+            };
+        
+            // DAR VALORES A UN CARTÓN
+            function rellenarCarton(&$carton, $ajustes){
+                while(count($carton) < $ajustes[2]){
+                    $num = rand(1, $ajustes[3]);
+                    if(!in_array($num, $carton)){
+                        array_push($carton,$num);
                     }
                 }
-				
-                echo "</tr> </table>";
+                // Añadir contador de bolas acertadas al final del cartón
+                array_push($carton, 0);
             }
-            echo "</div>";
-        }
-        echo "</div>";
-    }
-    /**************************************************/
 
-    // COMPRUEBA SI LA BOLA SALIDA SE ENCUENTRA EN ALGÚN CARTÓN
-    function comprobarBolaCartones(&$players, $bola) {
-        foreach ($players as $player => &$cartones)
-            foreach ($cartones as &$carton)
-                    for ($i = 0; $i < count($carton); $i++)
-                        if ($carton[$i] == $bola)
-                            $carton[$i] = "X";
-    }
+            /* ******************************** */
 
-    /**************************************************/
-
-    // COMPRUEBA SI ALGÚN JUGADOR HA GANADO
-    function comprobarGanador($players, &$ganador) {
-        $hayGanador = false;
-        foreach ($players as $player => &$cartones)
-            foreach ($cartones as &$carton) {
-                $contadorBolasCoincididas = 0;
-                for ($i = 0; $i < count($carton); $i++)
-                    if ($carton[$i] == "X")
-                        $contadorBolasCoincididas++;
-                if ($contadorBolasCoincididas == count($carton)) {
-                    $ganador .= $player . " ";
-                    $hayGanador = true;
+            // IMPRIMIR TODOS LOS CARTONES DE CADA JUGADOR
+            function imprimirCartones($players) {
+                echo "<div class='container'>";
+                foreach ($players as $player => $cartones) {
+                    echo "<div class='jugador'><h3>" . $player . " </h3>";
+                    foreach ($cartones as $num => $carton) {
+                        $num++;
+                        echo "<table border='1'><caption> Carton " . $num . "</caption><tr>";
+                        for ($i = 0; $i < count($carton)-1; $i++) {
+                            if ($i % 5 == 0 && $i != 0)
+                                echo "</tr> <tr> <td>" . $carton[$i] . "</td>";
+                            else
+                                echo "<td>" . $carton[$i] . "</td>";
+                        }
+                        echo "</tr> </table>";
+                    }
+                    echo "</div>";
                 }
+                echo "</div>";
             }
-        return $hayGanador;
-    }
-    ?>
-</body>
 
+            /* ******************************** */
+            
+            // CREAR BOLAS
+            function crearBolas(&$bolas, $ajustes) {
+                for ($i = 1; $i <= $ajustes[3]; $i++)
+                    array_push($bolas, $i);
+                shuffle($bolas);
+            }
+
+            /* ******************************** */
+
+            // INICIAR JUEGO
+            function iniciarJuego(&$players, &$bolas, $ajustes) {
+                $hayGanador = false;
+                $ganador = "";
+            
+                do {
+                    $hayGanador = sacarBola($players, $bolas, $ganador, $ajustes);
+                } while(!$hayGanador);
+                echo "<h1>Ganador: ". $ganador . "</h1>";
+            }
+        
+            // SACAR UNA BOLA Y COMPROBAR SI HAY GANADOR
+            function sacarBola(&$players, &$bolas, &$ganador, $ajustes) {
+                $num = array_shift($bolas);
+                echo "<img src='images/". $num . ".PNG' height='70'>";
+            
+                comprobarBolaCartones($players, $num);
+                $hayGanador = comprobarGanador($players, $ganador);
+            
+                return $hayGanador;
+            }
+        
+            // COMPROBAR SI LA BOLA QUE HA SALIDO SE ENCUENTRA EN ALGÚN CARTÓN
+            function comprobarBolaCartones(&$players, $bola) {
+                foreach ($players as $player => &$cartones)
+                    foreach ($cartones as &$carton)
+                        if (in_array($bola, $carton))
+                            $carton[count($carton) - 1]++;
+            }
+        
+            // COMPROBAR SI ALGÚN JUGADOR HA GANADO
+            function comprobarGanador($players, &$ganador) {
+                $hayGanador = false;
+                foreach ($players as $player => &$cartones)
+                    foreach ($cartones as &$carton)
+                        if ($carton[count($carton) - 1] == count($carton) - 1) {
+                            $ganador .= $player . " ";
+                            $hayGanador = true;
+                        }
+                return $hayGanador;
+            }
+        ?>
+    </body>
 </html>
